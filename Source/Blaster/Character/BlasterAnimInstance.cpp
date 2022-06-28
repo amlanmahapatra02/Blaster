774a4ab4c989fc88f4d905e3059b308698091f68
amlanmahapatra02 <amlanmahapatra02@gmail.com>
2022-06-28 23:52:29 +0530

    Character Movement Done for now

--- Source/Blaster/Character/BlasterAnimInstance.cpp
+++ Source/Blaster/Character/BlasterAnimInstance.cpp
@@ -2,4 +2,35 @@
 
 
 #include "BlasterAnimInstance.h"
+#include "BlasterCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
+void UBlasterAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	BlasterCharacter = Cast<ABlasterCharacter>(TryGetPawnOwner());
+}
+
+void UBlasterAnimInstance::NativeUpdateAnimation(float DeltaTime)
+{
+	Super::NativeUpdateAnimation(DeltaTime);
+
+	if (BlasterCharacter == nullptr)
+	{
+		BlasterCharacter = Cast<ABlasterCharacter>(TryGetPawnOwner());
+	}
+
+	if (BlasterCharacter)
+	{
+		FVector Velocity = BlasterCharacter->GetVelocity();
+		Velocity.Z = 0.0f;
+
+		Speed = Velocity.Size();
+
+		bIsInAir = BlasterCharacter->GetCharacterMovement()->IsFalling();
+
+		bisAccelerating = BlasterCharacter->GetCharacterMovement()->GetCurrentAcceleration().Size() > 0.0f ? true : false;
+
+	}
+}
--- Source/Blaster/Character/BlasterAnimInstance.h
+++ Source/Blaster/Character/BlasterAnimInstance.h
@@ -13,5 +13,22 @@ UCLASS()
 class BLASTER_API UBlasterAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaTime) override;
+
+private:
+	UPROPERTY(BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	class ABlasterCharacter* BlasterCharacter;
+
+	UPROPERTY(BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	float Speed;
+
+	UPROPERTY(BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	bool bIsInAir;
+
+	UPROPERTY(BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	bool bisAccelerating;
 	
 };
